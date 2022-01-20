6fc026ba8d47899c0c8d4a6023b632984f62105d
André Düwel <andre.duewel@gmail.com>
2022-01-20 21:35:02 +0100

    User Model created, GET and POST routes vreated

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const mongoose= require('mongoose');
 const bodyParser= require('body-parser');
 
 const User=require('./models/User');
-mongoose.connect('mongodb://localhost/userData')
+mongoose.connect('mongodb://localhost/userData');
 
 const port=8000;
 const app= express();
+
+console.log(User);
 
 app.use(bodyParser.json());
 
@@ -16,13 +18,43 @@ app.listen(port, ()=>{
 
 // CREATE
 app.post('/users',(req,res)=>{
-  // User.create()
-})
+  let user = new User({
+    name:req.body.newData.name,
+    email:req.body.newData.email,
+    password:req.body.newData.password
+  });
+  user.save()
+  .then((data) => {
+    console.log('User saved');  
+    res.json({success: true,data: data})
+  })
+  .catch(err => {
+    console.error(err)
+    res.json({success: false,message: err})
+  });
+})  
 
 app.route('/users/:id')
 // READ
 .get((req,res)=>{
-  // User.findById()
+  User.findById(req.params.id,(err,data)=>{
+    if (err){
+      res.json({
+        success: false,
+        message: err
+      })
+    } else if (!data){
+      res.json({
+        success: false,
+        message: "Not Found"
+      })
+    } else {
+      res.json({
+        success: true,
+        data: data
+      })
+    }
+  })
 })
 // UPDATE
 .put((req,res)=>{
